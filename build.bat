b7cb1da581fc37635cafa1e93dd01b6a31884515
nguyen1024 <nguyen1024@gmail.com>
2018-08-23 09:35:11 +1000

    Added comments.

--- build.bat
+++ build.bat
@@ -3,18 +3,23 @@ REM Deactivate printing commands to the screen to reduce clutter.
 REM This comment block comes after the command so that this comment block does
 REM not get printed to the screen.
 
-REM ###########################################################################
+REM ############################################################################
+REM ...
+REM ############################################################################
 
 REM \todo Figure out how to specify a build directory for cl.
+REM \todo Figure out how to not create a build directory if it already exists.
 
-REM ###########################################################################
+REM ############################################################################
 REM Create build directory.
+REM ############################################################################
 
 REM Create build directory.
 mkdir ..\build_dotnet_cpp_wrapper
 
-REM ###########################################################################
+REM ############################################################################
 REM Build .NET library.
+REM ############################################################################
 
 REM Go .NET library directory.
 cd YahooApi
@@ -25,14 +30,15 @@ csc /target:library /out:../../build_dotnet_cpp_wrapper/YahooApi.dll YahooAPI.cs
 REM Go to root directory.
 cd ..
 
-REM ###########################################################################
+REM ############################################################################
 REM Build wrapper.
+REM ############################################################################
 
 REM Go to wrapper directory.
 cd YahooApiWrapper
 
 REM Build wrapper.
-REM HOW TO SPECIFY THE USING DIRECTORY
+REM HOW TO SPECIFY THE "USING" DIRECTORY
 REM https://msdn.microsoft.com/en-us/library/x1x72k9t.aspx
 cl /clr /AI../../build_dotnet_cpp_wrapper /LD YahooAPIWrapper.cpp
 
@@ -47,8 +53,9 @@ del YahooAPIWrapper.obj
 REM Go to root directory.
 cd ..
 
-REM ###########################################################################
+REM ############################################################################
 REM Build application.
+REM ############################################################################
 
 REM Go to application directory.
 cd Test 
